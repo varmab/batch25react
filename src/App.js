08b5a0010fdf1b7049a42cb09da2f46b61964f7d
Varma Bhupatiraju <varmab@gmail.com>
2019-12-30 09:53:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome'
 import Counter from './Counter'
+import Friends from './Friends'
+import Todos from './Todos'
+import Users from './Users'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Welcome to My React</h1>
+          <Welcome title="Varma" year={2022} password="123456"/>
         </header>
-        <Counter/>
+        <Users/>
       </div>
     );
   }
